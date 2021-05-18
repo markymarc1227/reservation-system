9d031871a1da4775a392b9007b1f27f5dccd3a29
unknown <kristoffertumarao@gmail.com>
2021-05-18 09:39:13 +0800

    added register component

--- src/RenderRoute.js
+++ src/RenderRoute.js
@@ -1,9 +1,15 @@
 import React from "react";
+import UserSignIn from "./UserSignIn/UserSignIn";
+import UserRegister from "./UserRegister/UserRegister";
+
 
 const RenderRoute = ({route}) => {
   switch (route) {
     case 'signin':
-      return <div>Sign In</div>
+      return <UserSignIn/>
+      break;
+    case 'register':
+      return <UserRegister/>
       break;
     case 'userhome':
       return <div>User Home</div>;
@@ -12,23 +18,23 @@ const RenderRoute = ({route}) => {
       return <div>Pending Page</div>;
       break;
     case 'rescheduled':
-        return <div>Rescheduled</div>;
-        break;
+      return <div>Rescheduled</div>;
+      break;
     case 'checked':
-        return <div>Health Checklist</div>;
-        break;
+      return <div>Health Checklist</div>;
+      break;
     case 'confirmed':
-        return <div>Details</div>;
-        break;
+      return <div>Details</div>;
+      break;
     case 'adminsignin':
-        return <div>SignIn</div>;
-        break;
+      return <div>SignIn</div>;
+      break;
     case 'adminhome':
-        return <div>Admin Home</div>;
-        break;
+      return <div>Admin Home</div>;
+      break;
     default:
       return <div>Sign In</div>;
   }
 };
 
-export default AuthButton;
+export default RenderRoute;
